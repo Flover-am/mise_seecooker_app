1ec527696d380d949609a35b5ec667fc758af732
windloong <1299376070@qq.com>
2023-11-20 20:08:12 +0800

    fix:fix confict

--- lib/pages/post_page.dart
+++ lib/pages/post_page.dart
@@ -3,6 +3,9 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
+import 'package:seecooker/models/user_model.dart';
+import 'package:seecooker/pages/login_page.dart';
 
 class PostPage extends StatefulWidget {
   final String param;
@@ -38,6 +41,19 @@ class _PostPageState extends State<PostPage> {
   /// 主页面
   @override
   Widget build(BuildContext context) {
+    var userModel = Provider.of<UserModel>(context,listen: false);
+
+    // 检查用户是否已登录
+    if (!userModel.isLoggedIn) {
+      // 如果未登录，则导航到LoginPage
+      WidgetsBinding.instance!.addPostFrameCallback((_) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const LoginPage()),
+        );
+      });
+      print(userModel.isLoggedIn);
+      //Navigator.pop(context);
+    }
     return Scaffold(
       appBar: AppBar(
         actions: [
