2a72a0adaa9e01f9abf8742d0d41e0081df1d4f8
Lenovo <Lenovo@Lenovo.com>
2023-12-04 16:46:08 +0800

    feat/new_explore

---
+++ lib/services/explore_service.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:seecooker/models/comment.dart';
+
+import '../models/recipe.dart';
+
+class ExploreService {
+
+  static Future<List<Recipe>> fetchPosts() async {
+    await Future.delayed(const Duration(seconds: 1));
+    return List.generate(
+        5,
+            (index) => Recipe(
+          index,
+          '五红银耳羹',
+          'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+          'author$index',
+          114514,
+          3.2,
+        )
+    );
+  }
+
+  static Future<List<Recipe>> fetchMorePosts() async {
+    // TODO: ADD HTTP GET REQUEST
+    await Future.delayed(const Duration(seconds: 1));
+    return List.generate(
+        5,
+            (index) => Recipe(
+          index,
+          '黄焖鸡',
+          'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+          'author$index',
+          114514,
+          3.2,
+        )
+    );
+  }
+
+
+}
