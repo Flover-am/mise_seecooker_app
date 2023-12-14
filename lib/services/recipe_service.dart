78a397bb328e54aee7f784344a1743118c57a294
windloong <1299376070@qq.com>
2023-12-14 08:27:51 +0800

    fix:url change

--- lib/services/recipe_service.dart
+++ lib/services/recipe_service.dart
@@ -5,7 +5,7 @@ import 'package:seecooker/utils/shared_preferences_util.dart';
 
 class RecipeService {
   /// 测试阶段可以先用apiFox的Mock的url
-  static const String baseUrl = 'https://mock.apifox.com/m1/3614939-0-default';
+  static const String baseUrl = 'http://124.222.18.205:8080/v1';
 
   /// 使用Dio进行网络请求
   static Dio dio = Dio();
